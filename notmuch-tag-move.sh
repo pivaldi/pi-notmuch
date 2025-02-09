f30586964b5f4b675256e949dff1f381aa8530e0
pivaldi <git@ivaldi.me>
2025-02-09 21:12:20 +0100

    Remove excluding spam

--- notmuch-tag-move.sh
+++ notmuch-tag-move.sh
@@ -18,7 +18,7 @@ function moveToByFiler {
     filter="$1"
     toDir="$2"
 
-    files=$(notmuch search --output=files "$filter")
+    files=$(notmuch search --exclude=false --output=files -- "$filter")
     echo "notmuch search --output=files $filter"
     echo -n 'Number of impacted mails is '
     notmuch count -- "$filter"
@@ -31,10 +31,45 @@ function moveToByFiler {
     unset IFS
 }
 
-echo "Moving messages according to notmuch tags"
+function moveExpireToTrash {
+    account="$1"
+    trashDir="$account/trash"
+    echo "Account $account -- Moving emails tagged with expire and older than 90 days to ${trashDir}."
+    moveToByFiler "tag:$account AND tag:expire AND (NOT tag:deleted) AND (NOT tag:delete) AND date:-90d" "$trashDir"
+}
+
+function delete {
+    filter="$1"
+    files=$(notmuch search --exclude=false --output=files --format=text0 -- "$filter")
+    echo "Removing emails filtered by '${filter}'."
+    echo -n 'Number of impacted mails is '
+    notmuch count -- "$filter"
+    echo
+    # notmuch search --exclude=false --output=files --format=text0 -- "$filter" | xargs -r0 rm
+
+    IFS=$'\n'
+    for file in $files; do
+        echo -n "Removing file $file  "
+        rm "$file" && echo '[DONE]' || echo '[FAILED]'
+    done
+    unset IFS
+}
+
+function deleteFromTrash30d {
+    account="$1"
+    echo "Account $account -- Removing emails tagged deleted from 30 days."
+    delete "tag:$account AND tag:deleted AND date:-30d"
+}
+
+function synchronizeNotmuch {
+    echo "Synchronizing notmuch without hook"
+    notmuch new --no-hooks
+}
 
 # Make sure we have updated paths
-notmuch new --no-hooks
+synchronizeNotmuch
+
+echo "Moving messages according to notmuch tags"
 
 ###{{### Moving IVALDI.ME mails depending of notmuch tags ###
 DIR='ivaldi.me'
@@ -43,19 +78,16 @@ DIR_TRASH="$DIR/trash"
 DIR_ARCHIVE="$DIR/archive"
 
 echo 'Processing IVALDI.ME...'
-echo 'Removing emails tagged with expire and older than 90 days'
-notmuch search --output=files --format=text0 tag:ivaldi.me AND tag:expire AND date:-90d | xargs -r0 rm
+moveExpireToTrash "ivaldi.me"
+
+echo 'Moving archived mails'
+moveToByFiler "tag:ivaldi.me AND tag:archived AND (NOT folder:$DIR_ARCHIVE)" "$DIR_ARCHIVE"
 
 echo 'Moving emails tagged with delete to Trash'
 moveToByFiler "tag:delete AN (NOT tag:deleted) AND tag:ivaldi.me AND (NOT folder:$DIR_TRASH)" "$DIR_TRASH"
 
-## The mails in the Trash directory are tagged as deleted when syncing…
-## So after the next sync, they will be deleted.
-# echo 'Remove mails tagged as deleted'
-# notmuch search --output=files --format=text0 tag:ivaldi.me AND tag:deleted | xargs -r0 rm
-
-echo 'Moving archived mails'
-moveToByFiler "tag:ivaldi.me AND tag:archived AND (NOT folder:$DIR_ARCHIVE)" "$DIR_ARCHIVE"
+echo 'Remove mails tagged as deleted 30 days old'
+delete "tag:ivaldi.me AND tag:deleted AND date:-30d AND 'folder:\"$DIR_TRASH\"'"
 
 ###}}###
 
@@ -66,8 +98,7 @@ DIR_TRASH="$DIR/trash"
 DIR_ARCHIVE="$DIR/all"
 
 echo 'Processing OVYA.FR...'
-echo 'Removing emails tagged with expire and older than 90 days.'
-notmuch search --output=files --format=text0 tag:ovya.fr AND tag:expire AND date:-90d | xargs -r0 rm
+moveExpireToTrash "ovya.fr"
 
 echo 'Moving archived mails'
 moveToByFiler "tag:ovya.fr AND tag:archived AND (NOT tag:deleted) AND (NOT tag:delete) AND (NOT 'folder:\"${DIR_ARCHIVE}\"')" "$DIR_ARCHIVE"
@@ -76,14 +107,12 @@ echo 'Moving Gmail un-boxed mails to archive'
 moveToByFiler "tag:ovya.fr AND (NOT tag:archived) AND (NOT tag:inbox) AND (NOT tag:deleted) AND (NOT tag:delete) AND 'folder:\"${DIR_INBOX}\"'" "$DIR_ARCHIVE"
 
 echo 'Moving emails tagged with delete to Trash'
-moveToByFiler "tag:ovya.fr AND tag:delete AND (NOT tag:deleted) AND (NOT 'folder:\"$DIR_TRASH\"')" "$DIR_TRASH"
+moveToByFiler "tag:ovya.fr AND (NOT tag:archived) AND tag:delete AND (NOT tag:deleted) AND (NOT 'folder:\"$DIR_TRASH\"')" "$DIR_TRASH"
 
-## The mails in the Trash directory are tagged as deleted when syncing…
-## So after the next sync, they will be deleted.
-# echo 'Remove mails tagged as deleted'
-# notmuch search --output=files --format=text0 tag:ovya.fr AND tag:deleted | xargs -r0 rm
+echo 'Removing emails tagged deleted 30 days old.'
+delete "tag:ovya.fr AND tag:deleted AND date:-30d AND 'folder:\"$DIR_TRASH\"'"
 
 ###}}###
 
 # Make sure we have updated paths
-notmuch new --no-hooks
+synchronizeNotmuch
